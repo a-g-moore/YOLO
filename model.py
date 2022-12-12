5165f61e01962dc21958f1f12494e97249e455cc
Andrew Moore <andrewmoore@Andrews-MacBook-Air.local>
2022-12-12 16:04:06 -0600

    flexible model architecture & params using json. switched to lazy layers

--- model.py
+++ model.py
@@ -1,97 +1,73 @@
 import torch
 import torch.nn as nn
-
-# Architecture for the convolutional network
-architecture = [
-        {"type": "convolution", "kernel": 7, "outputChannels": 64, "stride": 2},
-        {"type": "pool", "kernel": 2, "stride": 2},
-        {"type": "convolution", "kernel": 3, "outputChannels": 192, "stride": 1},
-        {"type": "pool", "kernel": 2, "stride": 2},
-        {"type": "convolution", "kernel": 1, "outputChannels": 128, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 256, "stride": 1},
-        {"type": "convolution", "kernel": 1, "outputChannels": 256, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 512, "stride": 1},
-        {"type": "pool", "kernel": 2, "stride": 2},
-        {"type": "convolution", "kernel": 1, "outputChannels": 256, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 1, "outputChannels": 256, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 1, "outputChannels": 256, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 1, "outputChannels": 256, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 1, "outputChannels": 256, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 1, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 1024, "stride": 1},
-        {"type": "pool", "kernel": 2, "stride": 2},
-        {"type": "convolution", "kernel": 1, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 1024, "stride": 1},
-        {"type": "convolution", "kernel": 1, "outputChannels": 512, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 1024, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 1024, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 1024, "stride": 2},
-        {"type": "convolution", "kernel": 3, "outputChannels": 1024, "stride": 1},
-        {"type": "convolution", "kernel": 3, "outputChannels": 1024, "stride": 1},
-        ]
+import json
 
 # Class to package a convolution block, including batch norm
 class ConvolutionBlock(nn.Module):
-    def __init__(self, inputChannels, outputChannels, activation = nn.ReLU(), **kwargs):
+    def __init__(self, outputChannels, activation = nn.LeakyReLU(0.1), **kwargs):
         super(ConvolutionBlock, self).__init__()
-        self.block = nn.Conv2d(inputChannels, outputChannels, bias = False, **kwargs)
-        self.batchnorm = nn.BatchNorm2d(outputChannels)
+        self.block = nn.LazyConv2d(outputChannels, bias = False, **kwargs)
+        self.batchnorm = nn.LazyBatchNorm2d()
         self.activation = activation
 
     def forward(self, inputData):
         return self.activation(self.batchnorm(self.block(inputData)))
 
-class Yolo(nn.Module):
-    def __init__(self, architecture = architecture, inputChannels = 3, hiddenSize = 4096, splitSize = 7, convNet = None, **kwargs):
-        super(Yolo, self).__init__()
-        self.convNet = convNet if convNet else createConvNet(architecture, inputChannels)
-        self.fullNet = self._createFullNet(hiddenSize, splitSize)
+class LinearBlock(nn.Module):
+    def __init__(self, output_size, activation):
+        super(LinearBlock, self).__init__()
+        self.block = nn.LazyLinear(output_size)
+        self.activation = nn.LeakyReLU(0.1) if activation == "LeakyReLU" else nn.LeakyReLU(1)
 
     def forward(self, inputData):
-        return self.fullNet(self.convNet(inputData))
+        return self.activation(self.block(inputData))
 
-    def _createFullNet(self, hiddenSize, splitSize):
-        return nn.Sequential(
-                nn.Flatten(),
-                nn.Linear(splitSize * splitSize * 1024, hiddenSize),
-                nn.Dropout(0.0),
-                nn.LeakyReLU(0.1),
-                nn.Linear(hiddenSize, splitSize * splitSize * 25)
-                )
+class Model(nn.Module):
+    def __init__(self, classifier_name, featureDetector = None, **kwargs):
+        super(Model, self).__init__()
 
+        with open("architecture.json", "r") as FILE:
+            architectures = json.load(FILE)
 
+        self.featureDetector = featureDetector if featureDetector else createNetwork(architectures['feature_detector'])
+        self.classifier = createNetwork(architectures[classifier_name])
 
-def createLayer(inputChannels, description):
-    if description['type'] == "pool":
-        layer = nn.MaxPool2d(
+    def forward(self, inputData):
+        return self.classifier(self.featureDetector(inputData))
+
+def createLayer(description):
+    if description['type'] == "maxpool":
+        return  nn.MaxPool2d(
                 kernel_size = description['kernel'],
                 stride = description['stride'],
                 padding = 0
                 )
-        return (inputChannels, layer)
 
-    if description['type'] == "convolution":
-        layer = ConvolutionBlock(
-                inputChannels, 
+    if description['type'] == "avgpool":
+        return  nn.AvgPool2d(
+                kernel_size = description['kernel'],
+                stride = description['stride'],
+                padding = 0
+                )
+
+    if description['type'] == "conv":
+        return  ConvolutionBlock(
                 description['outputChannels'],
                 kernel_size = description['kernel'],
                 stride = description['stride'],
                 padding = int(description['kernel'] / 2)
                 )
-        return (description['outputChannels'], layer)
+    
+    if description['type'] == "linear":
+        return  LinearBlock(
+                description['output_size'],
+                description['activation']
+                )
+
+    if description['type'] == "flatten":
+        return nn.Flatten()
 
     print(f"Error: Cannot create layer type '{description['type']}'")
 
-def createConvNet(architecture, inputChannels):
-    layers = []
-    currentChannels = inputChannels
-    for layerDescription in architecture:
-        currentChannels, layer = createLayer(currentChannels, layerDescription)
-        layers.append(layer)
-
-    return nn.Sequential(*layers)
+def createNetwork(architecture):
+    return nn.Sequential(*[createLayer(description) for description in architecture])
